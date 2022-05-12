93a5f0ec67d9fd2c3724786857953edead246024
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-12 09:59:14 +0100

    BasicBullet now extends FlxSprite instead.

--- source/bullet/BasicBullet.hx
+++ source/bullet/BasicBullet.hx
@@ -1,17 +1,27 @@
 package bullet;
 
 import flixel.addons.weapon.FlxBullet;
+import flixel.math.FlxAngle;
 import flixel.util.FlxColor;
+import flixel.FlxSprite;
 
-class BasicBullet extends FlxBullet
+class BasicBullet extends FlxSprite
 {
-    public function new()
+    public var speed : Float = 100.0;
+
+    public function new(x:Float, y:Float)
     {
-        super();
+        super(x,y);
         trace("Bullet Created");
-        this.accelerates = true;
-        this.xAcceleration = 10;
-        this.yAcceleration = 10;
-        makeGraphic(10,10, FlxColor.WHITE);
+        //this.accelerates = false;
+        //this.xAcceleration = 1;
+        //this.yAcceleration = 1;
+        makeGraphic(40,40, flixel.util.FlxColor.WHITE);
+    }
+
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+        trace("Basic Bullet Updating");
     }
 }
