a6fc18446a3d9cb6fdc3a05db7522895f96fea8b
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-14 19:09:22 +0100

    Game now using crosshair.png instead of default mouse.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,6 +10,8 @@ import bullet.BasicBullet;
 import weapon.BasicWeapon;
 import flixel.input.mouse.FlxMouse;
 import flixel.group.FlxGroup;
+import shaders.ChromaticAbberationShader;
+
 
 class PlayState extends FlxState
 {
@@ -20,6 +22,7 @@ class PlayState extends FlxState
     var mouse  : FlxMouse;
     var bullets: FlxTypedGroup<BasicBullet>;
     var enemies: FlxTypedGroup<Enemy>;
+    var caShader : ChromaticAberrationShader;
 
     override public function create()
     {
@@ -43,12 +46,14 @@ class PlayState extends FlxState
 
         for(i in 0...100)
         {
-            var rx = Math.random() * 500;
-            var ry = Math.random() * 500;
+            var rx = 100 + Math.random() * 500;
+            var ry = 100 + Math.random() * 500;
             var e = new Enemy(rx,ry);
             this.enemies.add(e);
         }
         add(this.enemies);
+
+        FlxG.mouse.load("assets/images/Crosshair.png",4,-9,-9);
     }
 
     override public function update(elapsed:Float)
@@ -66,6 +71,7 @@ class PlayState extends FlxState
         FlxG.overlap(this.bullets, this.enemies, function(b,s)
                 {
                     s.kill();
+                    b.kill();
                     //FlxG.camera.shake(0.01,0.01);
                     //FlxG.camera.flash();
 
