992752c974fd3da1601e2a90ddd264918c94825b
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-14 18:09:49 +0100

    Added a very basic Enemy class.

---
+++ source/Enemy.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.addons.weapon.FlxBullet;
+import flixel.math.FlxAngle;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+class Enemy extends FlxSprite
+{
+    public var speed : Float = 100.0;
+    //public var angle : Float = 0.0;
+
+    public function new(x:Float, y:Float)
+    {
+        super(x,y);
+        trace("Enemy Created");
+        makeGraphic(20,20, flixel.util.FlxColor.RED);
+    }
+
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+        //trace("Basic Bullet Updating");
+        //this.x -= (speed * Math.cos(angle)) * elapsed;
+        //this.y -= (speed * Math.sin(angle)) * elapsed;
+    }
+}
