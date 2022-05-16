295b5412dc21befeec56a53f19fdc46213694c40
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-16 16:36:13 +0100

    Added Shadow instance to Man class.

--- source/Man.hx
+++ source/Man.hx
@@ -1,14 +1,24 @@
 package;
 
+import Shadow;
 import flixel.FlxSprite;
 
 class Man extends FlxSprite
 {
+    public var shadow : Shadow;
+
     public function new(x,y)
     {
         super(x,y);
         loadGraphic("assets/images/man.png", true, 16,16);
-        scale.set(3,3);
+        scale.set(2,2);
+
+        this.shadow = new Shadow(x, y - 30);
+        //this.stamp(shadow, 100,100);
+    }
+
+    override public function update(elapsed:Float)
+    {
     }
 }
 
