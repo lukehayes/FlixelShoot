9c269fd2707a25e29918dac1edd383226a0d4155
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-17 10:00:50 +0100

    Added room.Room base class.

---
+++ source/room/Room.hx
@@ -0,0 +1,13 @@
+package room;
+
+import flixel.FlxState;
+
+class Room extends FlxState
+{
+    public var name : String = "Base Room";
+
+    public function new()
+    {
+        super();
+    }
+}
