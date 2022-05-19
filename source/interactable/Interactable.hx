076bf73bc2f7b9c74d6a05d511ea3637c490b9b8
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-19 10:49:42 +0100

    Interactable.interact() now takes a state argument.

--- source/interactable/Interactable.hx
+++ source/interactable/Interactable.hx
@@ -1,5 +1,7 @@
 package interactable;
 
+import flixel.FlxState;
+
 /**
   Interface for anything that the player can interact with.
   **/
@@ -9,5 +11,5 @@ interface Interactable
       Trigger the interaction, show dialoge etc.
       @return void
       **/
-    public function interact() : Void;
+    public function interact(state:FlxState) : Void;
 }
--- source/interactable/Sign.hx
+++ source/interactable/Sign.hx
@@ -2,21 +2,31 @@ package interactable;
 
 import interactable.Interactable;
 import interactable.Interactive;
+import dialogue.DialogueBox;
 import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.FlxG;
+
+typedef FL = flixel.FlxG;
 
 class Sign extends Interactive implements Interactable
 {
     var text : String;
+    var db   : DialogueBox;
 
     public function new(text:String)
     {
         super();
         trace("Sign Created");
+        makeGraphic(100,100, flixel.util.FlxColor.RED);
         this.text = text;
+        var db = new DialogueBox(text);
+        //flixel.FlxG.state.add(this.db);
     }
 
-    public function interact() : Void
+    public function interact(state:FlxState) : Void
     {
-        trace(this.text);
+        FL.state.add(this.db);
+        trace(this.db.text);
     }
 }
