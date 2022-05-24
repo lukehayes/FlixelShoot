010763bfda4f7b692741c3662e0d07b5eb12fd9c
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-24 17:39:12 +0100

    Added start of HUD logic to update its values.

--- source/HUD.hx
+++ source/HUD.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.text.FlxText;
 import flixel.group.FlxGroup.FlxTypedGroup;
 
 /**
@@ -9,13 +10,33 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 class HUD extends FlxTypedGroup<FlxSprite>
 {
     public var zombieCount = 0;
-    public var zombieText  = 0;
+    public var zombieText:FlxText;
+    public var hasUpdated : Bool = false;
 
     public function new()
     {
         super();
-        var zombieText = new flixel.text.FlxText(0,0, "ZombieCount", 16);
+        var str= this.zombieCount + "";
+        this.zombieText = new flixel.text.FlxText(0,0, str, 11);
         zombieText.scrollFactor.set(0,0);
         add(zombieText);
+    }
+
+    override public function update(elapsed:Float)
+    {
+        //var str= this.zombieCount + "";
+        //var zombieText = new flixel.text.FlxText(0,0, str, 11);
+        //add(zombieText);
+
+        if(!hasUpdated)
+        {
+            remove(zombieText);
+            var str= this.zombieCount + "";
+            this.zombieText = new flixel.text.FlxText(0,0, str, 11);
+            add(zombieText);
+        }
+
+        hasUpdated = false;
+
     }
 }
